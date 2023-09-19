742ccc6133b9adeb4e3706cffa504a56e27381d9
Gianna <gvc222@nyu.edu>
2023-09-18 20:14:45 -0700

    final

--- src/App.js
+++ src/App.js
@@ -4,12 +4,23 @@ import Todo from "./components/Todo";
 import FilterButton from "./components/FilterButton";
 import {nanoid} from 'nanoid';
 
+
+const FILTER_MAP = {
+  All: () => true,
+  Active: (task) => !task.completed,
+  Completed: (task) => task.completed
+}
+const FILTER_NAMES = Object.keys(FILTER_MAP);
+
 function App(props) {
 
   const [tasks, setTasks] = useState(props.tasks);
+  const [filter, setFilter] = useState('All')
 
-  const taskList = tasks.map((task) => 
-    <li>
+  const taskList = tasks
+    .filter(FILTER_MAP[filter])
+    .map((task) => 
+
       <Todo 
       key={task.id} 
       id={task.id} 
@@ -17,8 +28,17 @@ function App(props) {
       completed={task.completed}
       toggleTaskCompleted={toggleTaskCompleted}
       deleteTask = {deleteTask}
+      editTask = {editTask}
       />
-    </li>);
+      );
+
+  const filterList = FILTER_NAMES.map((name) => (
+    <FilterButton 
+      key={name} 
+      name={name} 
+      isPressed={name === filter}
+      setFilter={setFilter}/>
+  ));
 
   function addTask(name) {
     const newTask = {id: `todo-${nanoid()}`, name, completed: false}
@@ -42,19 +62,28 @@ function App(props) {
   const tasksNoun = taskList.length !== 1 ? "tasks" : "task";
   const headingText = `${taskList.length} ${tasksNoun} remaining`;
   
-   
-  return (
+   function editTask(id, newName){
+    const editedTaskList = tasks.map((task) => {
+      if (id === task.id) {
+        return {...task, name: newName}
+      }
+      return task;
+    });
+    setTasks(editedTaskList);
+   }
+ 
+ 
+   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
         <Form onSubmit={addTask}/>
         <div className="filters btn-group stack-exception">
-          {/* <FilterButton />
-          <FilterButton />
-          <FilterButton /> */}
-      </div>
+          {filterList}
+        </div>
       <div className="tasks-remaining">
       <h2 id="list-heading">{headingText}</h2>
       </div>
+      
       <ul
         role="list"
         className="todo-list stack-large stack-exception"
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react'
 
 function Form(props) {
-  
   const [name, setName] = useState("");
   
   function handleSubmit(e) {
